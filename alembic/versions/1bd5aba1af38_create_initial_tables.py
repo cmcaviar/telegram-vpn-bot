215f568ad4aabf8c1748f5ee2e9b8488d8b5149a
Kirill Prosviryakov <kprosviryakov@alfabank.ru>
2025-02-03 07:46:56 +0300

    fix problems

--- alembic/versions/1bd5aba1af38_create_initial_tables.py
+++ alembic/versions/1bd5aba1af38_create_initial_tables.py
@@ -1,8 +1,8 @@
 """create_initial_tables
 
-Revision ID: 5c5ae8f445b3
-Revises: 
-Create Date: 2025-02-01 15:28:45.221255
+Revision ID: 2612ab29d0e5
+Revises: 5c5ae8f445b3
+Create Date: 2025-02-03 06:36:19.143962
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '5c5ae8f445b3'
+revision: str = '1bd5aba1af38'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -30,10 +30,28 @@ def upgrade() -> None:
         sa.Column('username', sa.Text),
         sa.Column('fullname', sa.Text)
     )
+
+    # Создание таблицы payments
+    op.create_table(
+        'payments',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('tgid', sa.BigInteger, nullable=False),
+        sa.Column('bill_id', sa.Text),
+        sa.Column('amount', sa.BigInteger),
+        sa.Column('time_to_add', sa.BigInteger),
+        sa.Column('mesid', sa.Text)
+    )
+
+    # Создание таблицы static_profiles
+    op.create_table(
+        'static_profiles',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('name', sa.String)
+    )
+
     print("Таблицы инициализированы.")
 
-
-    # Добавляем уникальный индекс для tgid с обработкой конфликтов
+    # Добавляем уникальный индекс для tgid
     op.create_index(
         'idx_userss_tgid_unique',
         'userss',
@@ -44,5 +62,7 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
-    # Удаление таблицы userss
+    # Удаление таблиц
+    op.drop_table('static_profiles')
+    op.drop_table('payments')
     op.drop_table('userss')
